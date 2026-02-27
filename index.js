86d3c860807ad59e1502ba860612c9188a963879
Maddela Archana <archanaarchu5757@gmail.com>
2026-02-27 23:10:49 +0530

    ready for render deployment

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ cron.schedule('0 0 * * *', async () => {
   timezone: "Asia/Kolkata" // Ensure this is exactly "Asia/Kolkata"
 });
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
